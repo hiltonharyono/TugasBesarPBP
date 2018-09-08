4d7e8b0321551a406574ce4707edd8e9d3d96ec4
Yudha Pratama <elitejr48@gmail.com>
2018-09-08 11:42:48 +0700

    design

--- app/src/main/java/tugasbesarpbp_f_keld/com/tugasbesarpbp_f_keld/RegisterActivity.java
+++ app/src/main/java/tugasbesarpbp_f_keld/com/tugasbesarpbp_f_keld/RegisterActivity.java
@@ -1,13 +1,20 @@
 package tugasbesarpbp_f_keld.com.tugasbesarpbp_f_keld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Register extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void Back(View view) {
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
