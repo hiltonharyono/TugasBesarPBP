1450e54cfce3fa44abf4f321aff9848b21b26609
hiltonharyono <43083921+hiltonharyono@users.noreply.github.com>
2018-09-08 11:06:55 +0700

    Initial commit

---
+++ app/src/main/java/tugasbesarpbp_f_keld/com/tugasbesarpbp_f_keld/HistoryCostumer.java
@@ -0,0 +1,13 @@
+package tugasbesarpbp_f_keld.com.tugasbesarpbp_f_keld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryCostumer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_costumer);
+    }
+}
